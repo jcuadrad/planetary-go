ac61c11b1ae7467d58a976c8372019b0e2f7582a
Glitch (planetary-go) <none>
2018-02-15 07:20:22 +0000

    🔫📹 Checkpoint sound.js:670300/147 index.html:670300/121

--- sound.js
+++ sound.js
@@ -4,7 +4,7 @@ var backingTrack = new Howl({
       src: ['https://cdn.glitch.com/d33f835c-f9e3-4d9b-a2fc-e8780d036b67%2FPlanetary%20(Go!)%20~%20Stems%20~%2014%20~%20Backing%20Tracks.mp3?1518505535158'],
       preload: true,
       onload: function() {
-        console.log('Finished!');
+        console.log('Finished Backing Track!');
       }
   });
 
@@ -12,23 +12,23 @@ var vocals = new Howl({
       src: ['https://cdn.glitch.com/d33f835c-f9e3-4d9b-a2fc-e8780d036b67%2FPlanetary%20(Go!)%20~%20Stems%20~%2001%20~%20Lead%20Vocal.mp3?1518505552897'],
       preload: true,
       onload: function() {
-        console.log('Finished!');
+        console.log('Finished Vocals!');
       }
   });
 
 var backingVocals = new Howl({
-      src: ['https://cdn.glitch.com/d33f835c-f9e3-4d9b-a2fc-e8780d036b67%2FPlanetary%20(Go!)%20~%20Stems%20~%2001%20~%20Lead%20Vocal.mp3?1518505552897'],
+      src: ['https://cdn.glitch.com/d33f835c-f9e3-4d9b-a2fc-e8780d036b67%2FPlanetary%20(Go!)%20~%20Stems%20~%2002%20~%20Backing%20Vocals.mp3?1518677841612'],
       preload: true,
       onload: function() {
-        console.log('Finished!');
+        console.log('Finished Backing Vocals!');
       }
   });
 
 var drums = new Howl({
-      src: ['https://cdn.glitch.com/d33f835c-f9e3-4d9b-a2fc-e8780d036b67%2FPlanetary%20(Go!)%20~%20Stems%20~%2001%20~%20Lead%20Vocal.mp3?1518505552897'],
+      src: ['https://cdn.glitch.com/d33f835c-f9e3-4d9b-a2fc-e8780d036b67%2FPlanetary%20(Go!)%20~%20Stems%20~%2003%20~%20Drums.mp3?1518677850598'],
       preload: true,
       onload: function() {
-        console.log('Finished!');
+        console.log('Finished Drums!');
       }
   });
 
@@ -39,11 +39,13 @@ AFRAME.registerComponent('listen', {
     element.addEventListener('mouseenter', function() {
       backingTrack.play();
       vocals.play();
+      drums.play();
+      backingVocals.play();
     });
   }
 });
 
-AFRAME.registerComponent('volume-backingTrack', {
+AFRAME.registerComponent('volume-backing-track', {
   init: function () {
     var element = this.el;
     
@@ -67,6 +69,34 @@ AFRAME.registerComponent('volume-vocals', {
     
     element.addEventListener('mouseleave', function() {
       vocals.mute(false);
+    });
+  }
+});
+
+AFRAME.registerComponent('volume-drums', {
+  init: function () {
+    var element = this.el;
+    
+    element.addEventListener('mouseenter', function() {
+      drums.mute(true);
+    });
+    
+    element.addEventListener('mouseleave', function() {
+      drums.mute(false);
+    });
+  }
+});
+
+AFRAME.registerComponent('volume-backing-vocals', {
+  init: function () {
+    var element = this.el;
+    
+    element.addEventListener('mouseenter', function() {
+      backingVocals.mute(true);
+    });
+    
+    element.addEventListener('mouseleave', function() {
+      backingVocals.mute(false);
     });
   }
 });
