eb473c41f4691e0764ced6732dc844eb25fe80c5
Glitch (the-super-hands) <none>
2018-02-15 06:03:26 +0000

    👟👯 Checkpoint sound.js:670300/234 index.html:670300/4

--- sound.js
+++ sound.js
@@ -1,18 +1,37 @@
-/* global AFRAME, THREE*/
+/* global AFRAME, THREE, Howl*/
+
+
 
 AFRAME.registerComponent('listen', {
   init: function () {
     var element = this.el;
     
-    element.addEventListener('click', function() {
-      element.emit('go', {}, true);
-      console.log('Emitting!')
-    });
+    var backing = new Howl({
+          src: ['https://cdn.glitch.com/d33f835c-f9e3-4d9b-a2fc-e8780d036b67%2FPlanetary%20(Go!)%20~%20Stems%20~%2014%20~%20Backing%20Tracks.mp3?1518505535158'],
+          preload: true,
+          onload: function() {
+            console.log('Finished!');
+            this.el.setAttribute('sound', {src: });
+          }
+      });
+    
+    //element.addEventListener('click', function() {
+     // element.emit('go', {}, true);
+     // console.log('Emitting!')
+   // });
   }
 });
 
 AFRAME.registerComponent('sound-ready', {
   init: function () {
+    var element = this.el;
+    
+    if (element.components.sound.loaded) {
+      console.log('Sound is ready bro!');
+    }
+  },
+  
+  update: function () {
     var element = this.el;
     
     if (element.components.sound.loaded) {
