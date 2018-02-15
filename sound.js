b46ff286d3f04c9795099444abddb1de75094841
Glitch (the-super-hands) <none>
2018-02-15 07:09:22 +0000

    🚒🕰 Checkpoint style.css:670300/185 index.html:670300/15 sound.js:670300/284

--- sound.js
+++ sound.js
@@ -1,6 +1,6 @@
 /* global AFRAME, THREE, Howl*/
 
-var backing = new Howl({
+var backingTrack = new Howl({
       src: ['https://cdn.glitch.com/d33f835c-f9e3-4d9b-a2fc-e8780d036b67%2FPlanetary%20(Go!)%20~%20Stems%20~%2014%20~%20Backing%20Tracks.mp3?1518505535158'],
       preload: true,
       onload: function() {
@@ -16,27 +16,43 @@ var vocals = new Howl({
       }
   });
 
+var backingVocals = new Howl({
+      src: ['https://cdn.glitch.com/d33f835c-f9e3-4d9b-a2fc-e8780d036b67%2FPlanetary%20(Go!)%20~%20Stems%20~%2001%20~%20Lead%20Vocal.mp3?1518505552897'],
+      preload: true,
+      onload: function() {
+        console.log('Finished!');
+      }
+  });
+
+var drums = new Howl({
+      src: ['https://cdn.glitch.com/d33f835c-f9e3-4d9b-a2fc-e8780d036b67%2FPlanetary%20(Go!)%20~%20Stems%20~%2001%20~%20Lead%20Vocal.mp3?1518505552897'],
+      preload: true,
+      onload: function() {
+        console.log('Finished!');
+      }
+  });
+
 AFRAME.registerComponent('listen', {
   init: function () {
     var element = this.el;
     
     element.addEventListener('mouseenter', function() {
-      backing.play();
+      backingTrack.play();
       vocals.play();
     });
   }
 });
 
-AFRAME.registerComponent('volume-backing', {
+AFRAME.registerComponent('volume-backingTrack', {
   init: function () {
     var element = this.el;
     
     element.addEventListener('mouseenter', function() {
-      backing.mute(true);
+      backingTrack.mute(true);
     });
     
     element.addEventListener('mouseleave', function() {
-      backing.mute(false);
+      backingTrack.mute(false);
     });
   }
 });
