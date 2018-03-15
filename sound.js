8f08fcd279e69333c36e137516d5b190f8ff4518
Glitch (planetary-go) <none>
2018-03-15 04:45:19 +0000

    🍫🚊 Add Load Screen & Start Screen

--- sound.js
+++ sound.js
@@ -1,10 +1,15 @@
-/* global AFRAME, THREE, Howl*/
+/* global AFRAME, THREE, Howl, backChange*/
+
+// Sound Loading
+
+var loadedSoundFiles = 0;
 
 var backingTrack = new Howl({
       src: ['https://cdn.glitch.com/d33f835c-f9e3-4d9b-a2fc-e8780d036b67%2FPlanetary%20(Go!)%20~%20Stems%20~%2014%20~%20Backing%20Tracks.mp3?1518505535158'],
       preload: true,
       onload: function() {
-        console.log('Finished Backing Track!');
+        loadedSoundFiles++
+        console.log('Finished Backing Track!', loadedSoundFiles);
       }
   });
 
@@ -12,7 +17,8 @@ var vocals = new Howl({
       src: ['https://cdn.glitch.com/d33f835c-f9e3-4d9b-a2fc-e8780d036b67%2FPlanetary%20(Go!)%20~%20Stems%20~%2001%20~%20Lead%20Vocal.mp3?1518505552897'],
       preload: true,
       onload: function() {
-        console.log('Finished Vocals!');
+        loadedSoundFiles++
+        console.log('Finished Vocals!', loadedSoundFiles);
       }
   });
 
@@ -20,29 +26,99 @@ var backingVocals = new Howl({
       src: ['https://cdn.glitch.com/d33f835c-f9e3-4d9b-a2fc-e8780d036b67%2FPlanetary%20(Go!)%20~%20Stems%20~%2002%20~%20Backing%20Vocals.mp3?1518677841612'],
       preload: true,
       onload: function() {
-        console.log('Finished Backing Vocals!');
+        loadedSoundFiles++
+        console.log('Finished Backing Vocals!', loadedSoundFiles);
       }
   });
 
-var drums = new Howl({
+var drums = new Howl({       
       src: ['https://cdn.glitch.com/d33f835c-f9e3-4d9b-a2fc-e8780d036b67%2FPlanetary%20(Go!)%20~%20Stems%20~%2003%20~%20Drums.mp3?1518677850598'],
       preload: true,
       onload: function() {
-        console.log('Finished Drums!');
+        loadedSoundFiles++
+        console.log('Finished Drums!', loadedSoundFiles);
       }
   });
+
+var assetsReady = () => {
+  var assets = document.querySelector('a-assets');
+  if (assets) {
+    console.log('Assets Ready', assets);
+    assets.addEventListener('loaded', function () {
+    console.log("OK LOADED");
+    });
+  } else {
+    console.log('Assets Not Ready');
+    return;
+  }
+}
+
+var soundReady = () => {
+  if (loadedSoundFiles === 4) {
+    var waitScreen = document.getElementById('wait');
+    console.log('Sound Is Ready', loadedSoundFiles);
+    clearInterval(checkSound)
+    waitScreen.innerHTML = `
+      <h1>LETS GO!<h1>
+    `
+    waitScreen.addEventListener('click', function() {
+      var startScreen = document.getElementById('start-screen');
+      var video = document.getElementById("title");
+      var html = `
+        <a-plane color="white" scale="0.071 0.071 0.705" material="shader:flat" position="0 1.292 -2.5" geometry="height:9;width:16" class="UIbutton" hoverable
+                   event-set__rescol="_event: click; color: #212121" listen>
+                   <a-text value="Let's Go!" color="#C6252A"></a-text>
+        </a-plane>
+      `;
+      startScreen.insertAdjacentHTML('beforeend', html);
+      waitScreen.remove();
+      clearInterval(backChange);
+      video.play();
+    });
+    /* var button = document.createElement('p')
+    button.innerHTML = 'Lets GO!'
+    waitScreen.appendChild(button);*/
+  } else {
+    console.log('Sound Not Ready', loadedSoundFiles);
+    return;
+  }
+}
+
+var execute = () => {
+  soundReady();
+  assetsReady();
+};
+
+var checkSound = setInterval(execute, 2000);
+
+// AFRAME Components
 
 AFRAME.registerComponent('listen', {
   init: function () {
     var element = this.el;
-    
-    element.addEventListener('hover-start', function() {
+    var startScreen = document.getElementById('start-screen');
+    var html = `
+    <a-animation id="fade" attribute="scale" to="0 0 0" dur="4000"></a-animation>
+    `
+    var play = () => {
       backingTrack.play();
       vocals.play();
       drums.play();
       backingVocals.play();
-      element.parentNode.removeChild(element);
-    });
+    }
+    
+    var add = () => {
+        element.addEventListener('click', function() {
+
+        startScreen.insertAdjacentHTML('beforeend', html);
+        //element.parentNode.removeChild(element);
+        startScreen.addEventListener('animationend', function() {
+          console.log('Animation Finished!')
+        });
+        var startMusic = setTimeout(play, 4000);
+      });
+    }
+    var addStart = setTimeout(add, 3000);
   }
 });
 
@@ -63,7 +139,7 @@ AFRAME.registerComponent('volume-backing-track', {
       })
       
       // Turn off Light
-      light.setAttribute('visible', false);
+      light.setAttribute('intensity', '0.1');
     });
     
     element.addEventListener('hover-end', function() {
@@ -77,7 +153,7 @@ AFRAME.registerComponent('volume-backing-track', {
       })
       
       // Turn on Light
-      light.setAttribute('visible', true);
+      light.setAttribute('intensity', '0.4');
     });
   }
 });
@@ -94,12 +170,12 @@ AFRAME.registerComponent('volume-vocals', {
       
       // Modify the color of the material
       obj.material = new THREE.MeshPhongMaterial({
-        color: 'grey',
+        emissive: '#004eff',
         flatShading: THREE.FlatShading
       })
       
       // Turn off Light
-      light.setAttribute('visible', false);
+      light.setAttribute('intensity', '0.1');
     });
     
     element.addEventListener('hover-end', function() {
@@ -113,7 +189,7 @@ AFRAME.registerComponent('volume-vocals', {
       })
       
       // Turn on Light
-      light.setAttribute('visible', true);
+      light.setAttribute('intensity', '0.4');
     });
   }
 });
@@ -130,12 +206,12 @@ AFRAME.registerComponent('volume-drums', {
       
       // Modify the color of the material
       obj.material = new THREE.MeshPhongMaterial({
-        color: 'grey',
+        emissive: '#ffea00',
         flatShading: THREE.FlatShading
       })
       
       // Turn off Light
-      light.setAttribute('visible', false);
+      light.setAttribute('intensity', '0.1');
     });
     
     element.addEventListener('hover-end', function() {
@@ -149,7 +225,7 @@ AFRAME.registerComponent('volume-drums', {
       })
       
       // Turn on Light
-      light.setAttribute('visible', true);
+      light.setAttribute('intensity', '0.4');
     });
   }
 });
@@ -166,12 +242,12 @@ AFRAME.registerComponent('volume-backing-vocals', {
       
       // Modify the color of the material
       obj.material = new THREE.MeshPhongMaterial({
-        color: 'grey',
+        emissive: '#72ff00',
         flatShading: THREE.FlatShading
       })
       
       // Turn off Light
-      light.setAttribute('visible', false);
+      light.setAttribute('intensity', '0.1');
     });
     
     element.addEventListener('hover-end', function() {
@@ -185,7 +261,7 @@ AFRAME.registerComponent('volume-backing-vocals', {
       })
       
       // Turn on Light
-      light.setAttribute('visible', true);
+      light.setAttribute('intensity', '0.4');
     });
   }
 });
---
+++ video.js
@@ -0,0 +1,15 @@
+/* global AFRAME, THREE */
+
+var colors = ["#C7242B", "#35B725", "#0B32BD", "#DECA38"]
+
+function colorOptions (array) {
+    var final = array[Math.floor(Math.random()*array.length)];
+  return final;
+} 
+
+function changeColor() {
+  var background = document.getElementById("wait");
+    background.style.backgroundColor = colorOptions(colors);
+}
+
+var backChange = setInterval(changeColor, 700) 
